c4cbe0ca6ca0060e6073f6ac51ea9a5e50c5106c
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-22 15:55:31 +0700

    add file

---
+++ BBH.BOS.Respository/MemberRepository.cs
@@ -0,0 +1,33 @@
+﻿using BBC.Core.WebService;
+using BBH.BOS.Domain.Entities;
+using BBH.BOS.Domain.Interfaces;
+using BBH.BOS.Shared;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBH.BOS.Respository
+{
+    public class MemberRepository : WCFClient<IIMemberService>, IIMemberService
+    {
+        static string pathLog = ConfigurationManager.AppSettings["PathLog"];
+        public MemberBO LoginAccount(string username, string password)
+        {
+            MemberBO objMemberBO = null;
+            string fileLog = Path.GetDirectoryName(Path.Combine(pathLog));
+            try
+            {
+                objMemberBO = Proxy.LoginAccount(username, password);
+            }
+            catch (Exception ex)
+            {
+                Utility.WriteLog(fileLog, ex.Message);
+            }
+            return objMemberBO;
+        }
+    }
+}
