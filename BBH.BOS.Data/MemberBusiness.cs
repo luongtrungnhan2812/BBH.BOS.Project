678c6f4b9c2836a33c251df3470eb5483809ca11
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-22 15:25:49 +0700

    add file

---
+++ BBH.BOS.Data/MemberBusiness.cs
@@ -0,0 +1,19 @@
+﻿using BBH.BOS.Domain.Entities;
+using BBH.BOS.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBH.BOS.Data
+{
+    public class MemberBusiness : IIMemberService
+    {
+        public MemberBO LoginManagerAccount(string username, string password)
+        {
+            MemberBO objMemberBO = new MemberBO();
+            return objMemberBO;
+        }
+    }
+}
