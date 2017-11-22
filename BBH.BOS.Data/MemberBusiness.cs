65a0ce1ebe878d8136a0e48205fddc080ca3cfa5
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-22 15:35:11 +0700

    add file

--- BBH.BOS.Data/MemberBusiness.cs
+++ BBH.BOS.Data/MemberBusiness.cs
@@ -1,19 +1,47 @@
-﻿using BBH.BOS.Domain.Entities;
+﻿using BBC.Core.Database;
+using BBH.BOS.Domain.Entities;
 using BBH.BOS.Domain.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace BBH.BOS.Data
 {
     public class MemberBusiness : IIMemberService
     {
+        public static string pathLog = ConfigurationManager.AppSettings["PathLog"];
         public MemberBO LoginAccount(string username, string password)
         {
-            MemberBO objMemberBO = new MemberBO();
-            return objMemberBO;
+            string fileLog = Path.GetDirectoryName(Path.Combine(pathLog, "Logs"));
+            Sqlhelper helper = new Sqlhelper("", "ConnectionString");
+            try
+            {
+                MemberBO objMemberBO = null;
+                //string sql = "select UserName,Password,ac.GroupID from admin a left join AccessRight ac on a.AdminID=ac.AdminID where UserName=@userName and Password=@pass and IsActive=1 and IsDelete=0";
+                string sql = "SP_LoginManagerAccount";
+                SqlParameter[] pa = new SqlParameter[2];
+                pa[0] = new SqlParameter("@userName", username);
+                pa[1] = new SqlParameter("@pass", password);
+
+                SqlCommand command = helper.GetCommand(sql, pa, true);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    objMemberBO = new MemberBO();
+                    objMemberBO.Email = reader["UserName"].ToString();
+                }
+                return objMemberBO;
+            }
+            catch (Exception ex)
+            {
+                Utilitys.WriteLog(fileLog, "Exception login admin : " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                helper.destroy();
+            }
         }
     }
 }
