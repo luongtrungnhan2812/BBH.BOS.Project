7e864f8956e9f9f98298d1f68021adc1e95d80ce
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-22 15:12:51 +0700

    add file

---
+++ BBH.BOS.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBH.BOS.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public DateTime CreateDate { get; set; }
+        public string CreateUser { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string UpdateUser { get; set; }
+        public DateTime DeleteDate { get; set; }
+        public string DeleteUser { get; set; }
+        public int IsActive { get; set; }
+        public int IsDelete { get; set; }
+    }
+}
---
+++ BBH.BOS.Domain/Entities/MemberBO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBH.BOS.Domain.Entities
+{
+    public class MemberBO : BaseEntity
+    {
+        public int MemberID { get; set; }
+        public string Email { get; set; }
+        public string E_Wallet { get; set; }
+        public string Password { get; set; }
+        public double Points { get; set; }
+        public int TotalRecord { get; set; }
+        public int TotalNewMember { get; set; }
+        public int TotalAllMember { get; set; }
+    }
+}
