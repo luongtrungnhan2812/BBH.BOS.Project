0f8227abd6193cb534f61f0ea498f50e69947c44
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-23 15:56:43 +0700

    add file

--- BBH.BOS.Web/Controllers/LoginController.cs
+++ BBH.BOS.Web/Controllers/LoginController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +11,7 @@ namespace BBH.BOS.Web.Controllers
 {
     public class LoginController : Controller
     {
+        string secretKey = ConfigurationManager.AppSettings["secretKey"];
         // GET: Login
         public ActionResult Index()
         {
@@ -20,6 +24,26 @@ namespace BBH.BOS.Web.Controllers
         public ActionResult ChangePassword()
         {
             return View();
+        }
+        [HttpPost]
+        public string LoginMember()
+        {
+            string result = "";
+            string strCapcha = Request["g-recaptcha-response"].ToString();
+            var response = strCapcha;
+            var client = new WebClient();
+            result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, response));
+            var obj = JObject.Parse(result);
+            var status = (bool)obj.SelectToken("success");
+            if (status == true)
+            {
+                result = "loginSuccess";
+            }
+            else
+            {
+                result = "loginfaile";
+            }
+            return result;
         }
     }
 }
