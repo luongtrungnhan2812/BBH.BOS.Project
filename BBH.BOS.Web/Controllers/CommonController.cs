9c1de6af704080b5457abfe4cb839debedbc8643
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-23 14:55:37 +0700

    ADD

--- BBH.BOS.Web/Controllers/CommonController.cs
+++ BBH.BOS.Web/Controllers/CommonController.cs
@@ -1,6 +1,8 @@
 ﻿using BBH.BOS.Shared;
+using NBitcoin;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,16 +11,30 @@ namespace BBH.BOS.Web.Controllers
 {
     public class CommonController : Controller
     {
+        string masterKey = ConfigurationManager.AppSettings["KeyBOS"];
         // GET: Common
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Deposit(string strWalletAddress = "18BVEnR3F3T4pwt83Yyu6q8w5F5Mf2hknx")
+        public ActionResult Deposit(string strIndexWallet = "3")
         {
-            ViewBag.WalletAddress = strWalletAddress;
-            string strQRCode = Utility.GenQRCode(strWalletAddress);
-            ViewBag.DataQRCode = "data:image/png;base64," + strQRCode + "";
+            try
+            {
+                string strClientExtKey = masterKey;
+                RandomUtils.Random = new UnsecureRandom();
+                ExtKey masterPubKey = new BitcoinExtKey(strClientExtKey, Network.TestNet);
+                ExtKey pubkey = masterPubKey.Derive(int.Parse(strIndexWallet), hardened: true);
+                var strWalletAddress = pubkey.PrivateKey.GetBitcoinSecret(Network.TestNet).GetAddress().ToString();
+                ViewBag.WalletAddress = strWalletAddress;
+                string strQRCode = Utility.GenQRCode(strWalletAddress);
+                ViewBag.DataQRCode = "data:image/png;base64," + strQRCode + "";
+            }
+            catch
+            {
+                ViewBag.WalletAddress = "00000000000000";
+                ViewBag.DataQRCode = "data:image/png;base64," + 00000000000000 + "";
+            }
             return PartialView();
         }
     }
