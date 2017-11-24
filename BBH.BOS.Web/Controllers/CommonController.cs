3fd1153880f9a29e842d15cb0b09b3254fb2972d
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-24 14:58:35 +0700

    add

--- BBH.BOS.Web/Controllers/CommonController.cs
+++ BBH.BOS.Web/Controllers/CommonController.cs
@@ -1,4 +1,5 @@
-﻿using BBH.BOS.Shared;
+﻿using BBH.BOS.Domain.Entities;
+using BBH.BOS.Shared;
 using NBitcoin;
 using System;
 using System.Collections.Generic;
@@ -17,18 +18,28 @@ namespace BBH.BOS.Web.Controllers
         {
             return View();
         }
-        public ActionResult DepositBTC(string strIndexWallet = "3")
+        public ActionResult DepositBTC()
         {
             try
             {
-                string strClientExtKey = masterKey;
-                RandomUtils.Random = new UnsecureRandom();
-                ExtKey masterPubKey = new BitcoinExtKey(strClientExtKey, Network.TestNet);
-                ExtKey pubkey = masterPubKey.Derive(int.Parse(strIndexWallet), hardened: true);
-                var strWalletAddress = pubkey.PrivateKey.GetBitcoinSecret(Network.TestNet).GetAddress().ToString();
-                ViewBag.WalletAddress = strWalletAddress;
-                string strQRCode = Utility.GenQRCode(strWalletAddress);
-                ViewBag.DataQRCode = "data:image/png;base64," + strQRCode + "";
+                if (Session["MemberInfomation"] != null)
+                {
+                    MemberInformationBO member = (MemberInformationBO)Session["MemberInfomation"];
+                    int intIndexWallet = member.IndexWallet;
+                    string strClientExtKey = masterKey;
+                    RandomUtils.Random = new UnsecureRandom();
+                    ExtKey masterPubKey = new BitcoinExtKey(strClientExtKey, Network.TestNet);
+                    ExtKey pubkey = masterPubKey.Derive(intIndexWallet, hardened: true);
+                    var strWalletAddress = pubkey.PrivateKey.GetBitcoinSecret(Network.TestNet).GetAddress().ToString();
+                    ViewBag.WalletAddress = strWalletAddress;
+                    string strQRCode = Utility.GenQRCode(strWalletAddress);
+                    ViewBag.DataQRCode = "data:image/png;base64," + strQRCode + "";
+                }
+                else
+                {
+                    ViewBag.WalletAddress = "00000000000000";
+                    ViewBag.DataQRCode = "data:image/png;base64," + 00000000000000 + "";
+                }
             }
             catch
             {
