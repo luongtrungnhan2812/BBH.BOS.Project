39674a11bcf95a67cbbc33272c324a76b303e250
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-27 11:57:35 +0700

    add

--- BBH.BOS.Web/Controllers/WalletController.cs
+++ BBH.BOS.Web/Controllers/WalletController.cs
@@ -1,7 +1,10 @@
 ﻿using BBH.BOS.Domain.Entities;
+using BBH.BOS.Domain.Interfaces;
+using Microsoft.Practices.Unity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +12,8 @@ namespace BBH.BOS.Web.Controllers
 {
     public class WalletController : Controller
     {
+        [Dependency]
+        protected ITransactionWalletService transactionWallet { get; set; }
         // GET: Wallet
         public ActionResult WalletBTC()
         {
@@ -21,7 +26,42 @@ namespace BBH.BOS.Web.Controllers
             {
                 ViewBag.NumberCoin = "0";
             }
+            ViewBag.strHtmlTransactionCoin = GenHtml_TractionCoin();
             return View();
+        }
+        public string GenHtml_TractionCoin()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            int memberID = -1;
+            try
+            {
+                IEnumerable<TransactionCoinBO> lstTransactionPointsBO = null;
+                if (Session["MemberInfomation"] != null)
+                {
+                    MemberInformationBO member = (MemberInformationBO)Session["MemberInfomation"];
+                    memberID = member.MemberID;
+                }
+                lstTransactionPointsBO = transactionWallet.ListTransactionWalletByMember(memberID, 1, 100);
+                if (lstTransactionPointsBO != null && lstTransactionPointsBO.Count() > 0)
+                {
+                    foreach (var item in lstTransactionPointsBO)
+                    {
+                        strBuilder.Append(@"<tr class='none-top-border'>");
+                        strBuilder.Append(@"<td> " + item.TransactionBitcoin + " </td>");
+                        strBuilder.Append(@"<td> " + item.WalletAddressID + " </td>");
+                        strBuilder.Append(@"<td>" + item.ValueTransaction + "</td>");
+                        strBuilder.Append(@"<td>" + item.CreateDate.ToString("dd/MM/yyyy") + "</td>");
+                        strBuilder.Append(@"</ tr >");
+                    }
+
+                }
+                return strBuilder.ToString();
+            }
+            catch
+            {
+                return "";
+            }
+
         }
     }
 }
