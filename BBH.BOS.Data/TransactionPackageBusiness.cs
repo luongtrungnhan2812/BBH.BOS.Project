54d2df8d56075d1d933460e9f4ed548e1ae0dc08
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-28 10:07:12 +0700

    add

---
+++ BBH.BOS.Data/TransactionPackageBusiness.cs
@@ -0,0 +1,18 @@
+﻿using BBH.BOS.Domain.Entities;
+using BBH.BOS.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBH.BOS.Data
+{
+    public class TransactionPackageBusiness: ITransactionPackageService
+    {
+        public bool InsertTransactionPackage(TransactionPackageBO objTransactionPackageBO)
+        {
+            return true;
+        }
+    }
+}
---
+++ BBH.BOS.Respository/TransactionPackageRepository.cs
@@ -0,0 +1,26 @@
+﻿using BBC.Core.WebService;
+using BBH.BOS.Domain.Entities;
+using BBH.BOS.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBH.BOS.Respository
+{
+    public class TransactionPackageRepository: WCFClient<ITransactionPackageService>, ITransactionPackageService
+    {
+        public bool InsertTransactionPackage(TransactionPackageBO objTransactionPackageBO)
+        {
+            try
+            {
+                return Proxy.InsertTransactionPackage(objTransactionPackageBO);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
