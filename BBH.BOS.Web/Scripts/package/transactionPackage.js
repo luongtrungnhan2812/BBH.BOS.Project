292cff50d058842e3a6adcacffb1054349b02ac0
luongtrungnhan2812 <luongtrungnhan2812@gmail.com>
2017-11-28 12:10:02 +0700

    add

---
+++ BBH.BOS.Web/Scripts/package/transactionPackage.js
@@ -0,0 +1,41 @@
+﻿$('#myTable > tbody  > tr').each(function () {
+    $(this).click(function () {
+        $("#packageId").val($(this).attr("data-id"));
+        if ($(this).attr("class") != "selected")
+        {
+            $('#myTable > tbody  > tr').each(function () {
+                $(this).removeClass("selected");
+            });
+            $(this).attr("class", "selected");
+        }
+    });
+});
+$('#btn_submit').on('click', function () {
+    //var CurrentPackageID = $('#myTable > tbody  > tr.checked').attr("data-id");
+    var ChosenPackageID = $('#myTable > tbody  > tr.selected').attr("data-id");
+    //if (CurrentPackageID >= ChosenPackageID)
+    //{
+    //    alert(CurrentPackageID);
+    //}
+    if (ChosenPackageID != '')
+    {
+        $.ajax({
+            type: "POST",
+            async: true,
+            url: "/Package/InsertTransactionPackage",
+            data: { packageID: ChosenPackageID},
+            beforeSend: function () {
+
+            },
+            success: function (d) {
+                if (d == 'success') {
+                    noty({ text: "Update Success", layout: "bottomRight", type: "information" });
+                    setTimeout(function () { window.location.reload(); }, 2000);
+                }
+            },
+            error: function (e) {
+
+            }
+        });
+    }
+});
