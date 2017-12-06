01f1c02a8558847b3e41baba916a0c7da9667c3e
phatbui91 <buitanphat1991@gmail.com>
2017-12-06 16:57:25 +0700

    6/12

--- BBH.BOS.Web/App_Start/RouteConfig.cs
+++ BBH.BOS.Web/App_Start/RouteConfig.cs
@@ -29,7 +29,7 @@ namespace BBH.BOS.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
